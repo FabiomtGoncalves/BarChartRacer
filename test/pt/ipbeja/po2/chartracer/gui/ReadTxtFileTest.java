85ce292e62d08765d029b6c80df40cf3c479fb66
João Portelinha <joaoportelinha@hotmail.com>
2022-06-01 15:40:50 +0100

    Test + Junit5

---
+++ test/pt/ipbeja/po2/chartracer/gui/ReadTxtFileTest.java
@@ -0,0 +1,12 @@
+package pt.ipbeja.po2.chartracer.gui;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReadTxtFileTest {
+    @Test
+    void testReadFile() {
+
+    }
+}
